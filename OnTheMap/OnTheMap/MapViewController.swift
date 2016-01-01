0ff9299ace5ffea8456134180403c8fdac447839
Rachel Schifano <rschifa1@gmail.com>
2015-12-31 22:35:19 -0600

    Initial commit

---
+++ OnTheMap/OnTheMap/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Rachel Schifano on 12/31/15.
+//  Copyright © 2015 schifano. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
