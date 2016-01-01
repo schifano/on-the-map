12e2276d6c004676e9b561ff604bca9f065c41c5
Rachel Schifano <rschifa1@gmail.com>
2015-12-31 22:57:12 -0600

    Add mapview

--- OnTheMap/OnTheMap/MapViewController.swift
+++ OnTheMap/OnTheMap/MapViewController.swift
@@ -10,17 +10,113 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Populate the dummy data
+        let locations = hardCodedLocationData()
+        var annotations = [MKPointAnnotation]()
+        
+        for dictionary in locations {
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let lon = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            
+            let firstName = dictionary["firstName"] as! String
+            let lastName = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(firstName) \(lastName)"
+            annotation.subtitle = mediaURL
+            
+            // Place annotation in an array
+            annotations.append(annotation)
+        }
+        // Add completed array of annotations to the map
+        self.mapView.addAnnotations(annotations)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // TODO: Add dummy data
+    /**
+    Returns the view associated with the specified annotation object.
+    - parameter mapView: The map view that requested the annotation view.
+    - parameter annotation: The object representing the annotation that is about to be displayed.
+    - returns: The annotation view to display for the specified annotation or nil to disaply a standard annotation view.
+    */
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseID) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            // create pin
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = UIColor.redColor()
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        return pinView
     }
-
-
+    
+    /**
+     Tells the delegate that the user tapped one of the annotation view's accessory buttons.
+     - parameter mapView: The map view containing the specified annotation view.
+     - parameter view: The annotation view whose button was tapped.
+     - parameter control: The control that was tapped.
+     */
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(NSURL(string: toOpen)!)
+            }
+        }
+    }
+    
+    // MARK: Sample hardcoded data. An array of dictionaries. This would be JSON data.
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
+    }
 }
-
