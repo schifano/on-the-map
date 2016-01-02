1c9e68fe054c1419a61aaacccb441f37cc157635
Rachel Schifano <rschifa1@gmail.com>
2016-01-01 23:38:38 -0600

    Add email and pw textfields

---
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Rachel Schifano on 12/31/15.
+//  Copyright © 2015 schifano. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController {
+    
+}
