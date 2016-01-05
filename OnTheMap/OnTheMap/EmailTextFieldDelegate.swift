2f69c4bd683d320d2543940c92210e34abf59ffd
Rachel Schifano <rschifa1@gmail.com>
2016-01-04 23:57:11 -0600

    Create initial textfielddelegate classes

---
+++ OnTheMap/OnTheMap/EmailTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  EmailTextFieldDelegate.swift
+//  OnTheMap
+//
+//  Created by Rachel Schifano on 1/4/16.
+//  Copyright © 2016 schifano. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class EmailTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
---
+++ OnTheMap/OnTheMap/PasswordTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  PasswordTextFieldDelegate.swift
+//  OnTheMap
+//
+//  Created by Rachel Schifano on 1/4/16.
+//  Copyright © 2016 schifano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PasswordTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
