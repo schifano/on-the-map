7a40a7b6075c86c1958361f3ff74be7e064b305a
Rachel Schifano <rschifa1@gmail.com>
2016-01-12 23:10:14 -0600

    Add method URLs

--- OnTheMap/OnTheMap/UdacityConstants.swift
+++ OnTheMap/OnTheMap/UdacityConstants.swift
@@ -10,5 +10,18 @@ import UIKit
 import Foundation
 
 extension UdacityClient {
+    struct Constants {
+        // MARK: API Keys
+        static let ParseAppID: String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let RestApiKey: String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        
+        // MARK: URLs
+        static let BaseURLSecure: String = "https://udacity.com"
+    }
     
+    struct Methods {
+        static let SessionAuthentication: String = "https://www.udacity.com/api/session"
+        static let UserData: String = "https://www.udacity.com/api/users/"
+        static let StudentLocation: String = "https://api.parse.com/1/classes/StudentLocation"
+    }
 }
