5978f243aabb5d88fa6b513109e79eda3c1d9d98
Rachel Schifano <rschifa1@gmail.com>
2016-01-17 23:23:53 -0600

    Add appdelegate and session variables

--- OnTheMap/OnTheMap/LoginViewController.swift
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -14,12 +14,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    var appDelegate: AppDelegate!
+    var session: NSURLSession!
+    
     // TODO: Add constraints and aesthetics to text fields
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* Get the app delegate */
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         
-        
-        
-        
+        /* Get the shared URL session */
+        session = NSURLSession.sharedSession()
     }
 }
