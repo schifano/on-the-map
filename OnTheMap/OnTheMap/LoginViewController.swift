6b92c8ad8c8875d8f2486c214ebac640aa91bc9b
Rachel Schifano <rschifa1@gmail.com>
2016-02-02 15:00:51 -0600

    Configure UI with gradient and text fields

--- OnTheMap/OnTheMap/LoginViewController.swift
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -9,13 +9,15 @@
 import Foundation
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     var appDelegate: AppDelegate!
     var session: NSURLSession!
+    
+    var backgroundGradient: CAGradientLayer? = nil
     
     // TODO: Add constraints and aesthetics to text fields
     override func viewDidLoad() {
@@ -25,5 +27,26 @@ class LoginViewController: UIViewController {
         
         /* Get the shared URL session */
         session = NSURLSession.sharedSession()
+        
+        /* Configure the UI */
+        self.configureUI()
+    }
+}
+
+// TODO: Dismiss keyboard
+
+// MARK: LoginViewController (ConfigureUI)
+extension LoginViewController {
+    func configureUI() {
+        /* Configure Background Gradient */
+        // set background color to clear so that the gradient can be drawn on top of it
+        self.view.backgroundColor = UIColor.clearColor()
+        let colorTop = UIColor(red: 1.0, green: 0.67, blue: 0.2, alpha: 1.0).CGColor
+        let colorBottom = UIColor(red: 0.886, green: 0.37, blue: 0.05, alpha: 1.0).CGColor
+        backgroundGradient = CAGradientLayer()
+        backgroundGradient!.colors = [colorTop, colorBottom]
+        backgroundGradient!.locations = [0.0, 1.0]
+        backgroundGradient!.frame = view.frame
+        self.view.layer.insertSublayer(backgroundGradient!, atIndex: 0)
     }
 }
---
+++ OnTheMap/OnTheMap/UdacityConvenience.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityConvenience.swift
+//  OnTheMap
+//
+//  Created by Rachel Schifano on 1/26/16.
+//  Copyright © 2016 schifano. All rights reserved.
+//
+
+import Foundation
