8426f20eaaa150362cab590da62057ed04d2c9bf
Rachel Schifano <rschifa1@gmail.com>
2016-02-07 20:43:20 -0600

    Remove extension for escapedParameters

--- OnTheMap/OnTheMap/LoginViewController.swift
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -182,27 +182,3 @@ extension LoginViewController {
         return keyboardSize.CGRectValue().height
     }
 }
-
-extension LoginViewController {
-    
-    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
-    func escapedParameters(parameters: [String : AnyObject]) -> String {
-        
-        var urlVars = [String]()
-        
-        for (key, value) in parameters {
-            
-            /* Make sure that it is a string value */
-            let stringValue = "\(value)"
-            
-            /* Escape it */
-            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
-            
-            /* Append it */
-            urlVars += [key + "=" + "\(escapedValue!)"]
-            
-        }
-        
-        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
-    }
-}
