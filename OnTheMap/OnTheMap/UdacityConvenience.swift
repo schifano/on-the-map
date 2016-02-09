e46e5fc0998ef0775975558ce9cbce2b4ff12cc0
Rachel Schifano <rschifa1@gmail.com>
2016-02-09 15:58:57 -0600

    Add loginWithUdacity method

--- OnTheMap/OnTheMap/UdacityConvenience.swift
+++ OnTheMap/OnTheMap/UdacityConvenience.swift
@@ -13,9 +13,46 @@ import UIKit
 
 extension UdacityClient {
     
-    func authenticateWithUdacity(username: String, password: String, completionHandler: (success: Bool, errorString: NSError?) -> Void) {
+    func authenticateWithUdacity(username: String, password: String, completionHandler: (success: Bool, userID: String?, errorString: NSError?) -> Void) {
      
         print("reached authenticateWithUdacity method")
-        
+        // login to udacity using username and password instead of token
+        loginWithUdacity(username, password: password) { (success, userID, error) in
+            if success {
+                // unwrap optional value returned
+                if let userID = userID {
+                    // Store the returned userID
+                    UdacityClient.StudentInformation.userID = userID
+                }
+            }
+        }
     }
+    
+    func loginWithUdacity(username: String, password: String, completionHandler: (success: Bool, userID: String?, errorString: NSError?) -> Void) {
+        
+        /* 1. Specify parameters, method (if has {key}), and HTTP body (if POST) */
+        let userInfo: [String: String] = [UdacityClient.JSONBodyKeys.Username: username, UdacityClient.JSONBodyKeys.Password: password]
+        let jsonBody: [String: AnyObject] = [UdacityClient.JSONBodyKeys.Udacity: userInfo]
+        
+        /* 2. Make the request */
+        taskForPOSTMethod(UdacityClient.Methods.Session, parameters: nil, jsonBody: jsonBody) { JSONResult, error in
+            
+            /* 3. Send the desired value(s) to completion handler */
+            if let error = error {
+                completionHandler(success: false, userID: nil, errorString: error)
+            } else {
+                if let results = JSONResult[UdacityClient.JSONResponseKeys.UserKey] as? [String: AnyObject] {
+                    let userID = results[UdacityClient.JSONResponseKeys.UserKey] as? String
+                    completionHandler(success: true, userID: userID, errorString: nil)
+                } else {
+                    completionHandler(success: true, userID: nil, errorString: NSError(domain: "Login parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse loginWithUdacity"]))
+                }
+            }
+        }
+    }
+    
+//    func getPublicUserData(completionHandler: (success: Bool, firstName: String?, lastName: String?, error: NSError?) -> Void {
+//        
+//    }
+    
 }
