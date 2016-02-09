ac9fc121ef03389d4464fcb5c8278cb3215400e5
Rachel Schifano <rschifa1@gmail.com>
2016-02-09 16:50:29 -0600

    Add getPublicUserData method

--- OnTheMap/OnTheMap/UdacityConvenience.swift
+++ OnTheMap/OnTheMap/UdacityConvenience.swift
@@ -51,8 +51,30 @@ extension UdacityClient {
         }
     }
     
-//    func getPublicUserData(completionHandler: (success: Bool, firstName: String?, lastName: String?, error: NSError?) -> Void {
-//        
-//    }
+    func getPublicUserData(completionHandler: (success: Bool, firstName: String?, lastName: String?, error: NSError?) -> Void) {
+        /* 1. Specify the parameters, method (if has {key}), and HTTP body (if POST) */
+        var mutableUserMethod: String = UdacityClient.Methods.Users
+        mutableUserMethod = UdacityClient.substituteKeyInMethod(mutableUserMethod, key: UdacityClient.URLKeys.UserID, value: String(UdacityClient.StudentInformation.userID))!
+        
+        /* 2. Make the request */
+        taskForGETMethod(mutableUserMethod, parameters: nil) { JSONResult, error in
+            
+            /* 3. Send the desired value(s) to completion handler */
+            if let error = error {
+                completionHandler(success: false, firstName: nil, lastName: nil, error: error)
+            } else {
+                if let results = JSONResult[UdacityClient.JSONResponseKeys.User] as? [String: AnyObject]{
+                    let firstName = results[UdacityClient.JSONResponseKeys.FirstName] as? String
+                    let lastName = results[UdacityClient.JSONResponseKeys.LastName] as? String
+                    completionHandler(success: true, firstName: firstName, lastName: lastName, error: nil)
+                } else {
+                    completionHandler(success: false, firstName: nil, lastName: nil, error: NSError(domain: "getPublicUserData", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse results"]))
+                }
+            }
+        }
+    }
+    
+    
+    
     
 }
